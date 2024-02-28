588d2360daf9bbc2c7acdf4a095030c27d5de198
afosh sharif <afoshidris@gmail.com>
2024-02-28 23:36:40 +0300

    CI: updated release configurations

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,13 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ["master", "next"],
+  branches: ["master", "main"],
   repositoryUrl: "https://github.com/afosh/architecture.git",
+  GITHUB_TOKEN: "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",
+  GH_TOKEN: "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",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
 };
